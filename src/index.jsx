385686678b09c1106eb74b0582b6a22d143fd336
DavidFranco3 <josedavidayalafranco3@gmail.com>
2023-02-13 23:49:20 -0600

    se migra a react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +11,17 @@ import "react-datepicker/dist/react-datepicker.css";
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+
+root.render(
+    <StrictMode>
+        <App tab="home" />
+    </StrictMode>,
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
