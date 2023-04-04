eda3d4e281e87da88439691fc2f366e61aff462c
DavidFranco3 <josedavidayalafranco3@gmail.com>
2023-04-04 16:19:35 -0600

    correcciones

--- src/components/TerminalPV/Tiquet/Tiquet.jsx
+++ src/components/TerminalPV/Tiquet/Tiquet.jsx
@@ -449,20 +449,19 @@ function Tiquet(props) {
     const Opciones = ({ icon }) => {
         return (
             <div className="ticket__actions">
-                <Button className="button__options" title="Registrar venta" onClick={() => handleRegistraVenta()}>✅</Button>
+                <button title="Registrar venta" onClick={() => handleRegistraVenta()}>✅</button>
 
-                <Button className="button__options" title="Imprimir ticket único" onClick={() => handlePrint()}>📄</Button>
+                <button title="Imprimir ticket único" onClick={() => handlePrint()}>📄</button>
 
-                <Button className="button__options" title="Imprimir doble ticket" onClick={() => handlePrintDouble()}> 2️⃣</Button>
+                <button title="Imprimir doble ticket" onClick={() => handlePrintDouble()}> 2️⃣</button>
 
-                <Button className="button__options" title="Limpiar el ticket" onClick={() => handleEmptyTicket()}>🗑️</Button>
+                <button title="Limpiar el ticket" onClick={() => handleEmptyTicket()}>🗑️</button>
 
-                <Button className="button__options" title="Aplicar IVA" onClick={() => handleIVAApply()}>🧾</Button>
+                <button title="Aplicar IVA" onClick={() => handleIVAApply()}>🧾</button>
 
-                <Button className="button__options" title="Cancelar IVA" onClick={() => handleIVACancel()}>🚫️</Button>
+                <button title="Cancelar IVA" onClick={() => handleIVACancel()}>🚫️</button>
 
-                <Button
-                    className="button__options"
+                <button
                     title="Añadir detalles de la venta"
                     onClick={() =>
                         datosExtraVenta(
@@ -486,7 +485,7 @@ function Tiquet(props) {
                         )
                     }>
                     <FontAwesomeIcon icon={icon} />
-                </Button>
+                </button>
 
                 {/*<Button href="whatsapp://send?text=Hola Mundo&phone=+524531527363">Enviar mensaje</Button>*/}
 
