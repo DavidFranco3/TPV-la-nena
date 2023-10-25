c2eaf938c41c3376c63dd76ec65a3f37e1eda5c7
diegoisotechm <diego94.gonzalez26@gmail.com>
2023-10-25 11:27:32 -0600

    se corrige el cloudinary.js ya que al modificar producto se quedaba pensando. en getTime()

--- src/api/cloudinary.js
+++ src/api/cloudinary.js
@@ -1,22 +1,27 @@
+// Importar constantes y módulos necesarios
 import { API_CLOUDINARY } from "../utils/constants";
-
 import axios from "axios";
 
-// Guardar archivos en cloudinary
+// Función para subir archivos a Cloudinary
 export async function subeArchivosCloudinary(imagen, carpeta) {
 
+    // Crear un nuevo objeto FormData
     const data = new FormData()
-    data.append("file", imagen)
-    data.append("upload_preset", "TPV_LA_NENA")
-    data.append('public_id', `${carpeta}/${Date.now().getTime() + "_" + imagen.name}`)
-    data.append('folder', `${carpeta}`)
-    data.append("cloud_name", "omarlestrella")
 
+    // Agregar la imagen y los parámetros necesarios al objeto FormData
+    data.append("file", imagen) // Agregar la imagen al FormData
+    data.append("upload_preset", "TPV_LA_NENA") // Agregar el preset de carga
+    data.append('public_id', `${carpeta}/${Date.now() + "_" + imagen.name}`) // Agregar un ID público único para la imagen
+    data.append('folder', `${carpeta}`) // Agregar la carpeta en la que se almacenará la imagen
+    data.append("cloud_name", "omarlestrella") // Agregar el nombre de la nube de Cloudinary
+
+    // Configuración de encabezados para la solicitud
     const config = {
         headers: {
-            'Content-Type': 'multipart/form-data'
+            'Content-Type': 'multipart/form-data' // Especificar el tipo de contenido como multipart/form-data
         }
     };
 
+    // Realizar la solicitud POST a la API de Cloudinary con los datos y la configuración
     return await axios.post(API_CLOUDINARY, data, config);
 }
